b344fc7a615464421a1182a9d604955d0b82d135
Taynan Konrath <taynankonrath@gmail.com>
2024-01-08 11:34:57 -0300

    Tratando erros

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,15 +5,24 @@ namespace xadrez_console
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
-            Tabuleiro t = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro t = new Tabuleiro(8, 8);
 
-            t.ColocarPeca(new Torre(t, Cor.Preta), new Posicao(0, 0));
-            t.ColocarPeca(new Torre(t, Cor.Preta), new Posicao(1, 3));
-            t.ColocarPeca(new Rei(t, Cor.Preta), new Posicao(2, 4));
+                t.ColocarPeca(new Torre(t, Cor.Preta), new Posicao(0, 0));
+                t.ColocarPeca(new Torre(t, Cor.Preta), new Posicao(1, 3));
+                t.ColocarPeca(new Rei(t, Cor.Preta), new Posicao(0, 0));
 
-            Tela.ImprimirTabuleiro(t);
+                Tela.ImprimirTabuleiro(t);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
